faaa5a1a7a55832e2775512715261b83914a1112
Mrrebrik <itibsoft@gmail.com>
2023-09-20 02:00:13 +0700

    [Project] Update panel atribute

--- Scripts/Common/PanelAttribute.cs
+++ Scripts/Common/PanelAttribute.cs
@@ -1,11 +1,9 @@
-﻿using System;
-using JetBrains.Annotations;
+﻿using JetBrains.Annotations;
 using UnityEngine.Scripting;
 
 namespace Itibsoft.PanelManager
 {
     [MeansImplicitUse(ImplicitUseKindFlags.Assign)]
-    [AttributeUsage(AttributeTargets.Constructor | AttributeTargets.Method | AttributeTargets.Property | AttributeTargets.Field | AttributeTargets.Parameter, AllowMultiple = false)]
     public class PanelAttribute : PreserveAttribute
     {
         public PanelType PanelType;
