95fb47404617be336cbd6a0c6821cba1ede9a1cd
Mrrebrik <itibsoft@gmail.com>
2023-10-07 01:15:02 +0700

    [Project] [Release] Create example for panel manager v0.0.3

---
+++ Example~/Scripts/DemoWindow/DemoWindowController.cs
@@ -0,0 +1,40 @@
+﻿using Itibsoft.PanelManager;
+using UnityEngine;
+
+namespace Example.Demo
+{
+    [Panel(PanelType = PanelType.Window, AssetId = "DemoWindow/Prefabs/DemoWindow.prefab")]
+    public class DemoWindowController : PanelControllerBase<DemoWindow>
+    {
+        public DemoWindowController(DemoWindow panel) : base(panel)
+        {
+            panel.OnClick += OnClickHandle;
+            panel.OnCloseWindow += OnCloseWindowHandle;
+            panel.OnReleaseWindow += OnReleaseWindowHandle;
+        }
+        
+        private void OnClickHandle()
+        {
+            Debug.Log("DemoWindowController.Click");
+        }
+        
+        private void OnCloseWindowHandle()
+        {
+            Debug.Log("DemoWindowController.Close");
+            Close();
+        }
+        
+        private void OnReleaseWindowHandle()
+        {
+            Debug.Log("DemoWindowController.Release");
+            Release();
+        }
+
+        public override void Dispose()
+        {
+            base.Dispose();
+            
+            Debug.Log("DemoWindowController.Dispose");
+        }
+    }
+}
---
+++ Example~/Scripts/StartApp.cs
@@ -0,0 +1,19 @@
+﻿using Itibsoft.PanelManager;
+using UnityEngine;
+
+namespace Example.Demo
+{
+    public class StartApp : MonoBehaviour
+    {
+        private void Awake()
+        {
+            var panelManager = PanelManagerBuilder
+                .Create()
+                .Build();
+
+            var demoWindowController = panelManager.LoadPanel<DemoWindowController>();
+            
+            demoWindowController.Open();
+        }
+    }
+}
