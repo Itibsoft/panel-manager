34cffcb3b87f250211e97d17551f55071e657013
Mrrebrik <itibsoft@gmail.com>
2023-11-11 23:23:44 +0700

    - create primitive panel manager builder

---
+++ Scripts/Builder/PanelManagerBuilder.cs
@@ -0,0 +1,33 @@
+﻿namespace Itibsoft.PanelManager
+{
+    public class PanelManagerBuilder
+    {
+        private PanelDispatcher _panelDispatcher;
+        private IPanelControllerFactory _panelControllerFactory;
+        
+        public static PanelManagerBuilder Create()
+        {
+            return new PanelManagerBuilder();
+        }
+
+        public PanelManagerBuilder SetPanelDispatcher(PanelDispatcher panelDispatcher)
+        {
+            _panelDispatcher = panelDispatcher;
+
+            return this;
+        }
+
+        public PanelManagerBuilder SetPanelControllerFactory(IPanelControllerFactory panelControllerFactory)
+        {
+            _panelControllerFactory = panelControllerFactory;
+
+            return this;
+        }
+
+        public IPanelManager Build()
+        {
+            var panelManager = new PanelManager(_panelControllerFactory, _panelDispatcher);
+            return panelManager;
+        }
+    }
+}
