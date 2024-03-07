71206a372f7110de756a50bbcc1f49fefb6c7565
Mrrebrik <itibsoft@gmail.com>
2024-03-08 03:44:16 +0700

    - feat: create reflex implementation

---
+++ Scripts/External/Reflex/PanelManagerInstaller.cs
@@ -0,0 +1,33 @@
+﻿#if REFLEX
+
+using Reflex.Core;
+using UnityEngine;
+
+namespace Itibsoft.PanelManager.External
+{
+    public class PanelManagerInstaller : MonoBehaviour, IInstaller
+    {
+        [SerializeField] private PanelDispatcher _panelDispatcher;
+        
+        public void InstallBindings(ContainerBuilder containerBuilder)
+        {
+            var panelFactory = new ResourcesPanelFactory();
+            var panelControllerFactory = new ReflexPanelControllerFactory(panelFactory);
+            
+            var panelManager = PanelManagerBuilder
+                .Create()
+                .SetPanelDispatcher(_panelDispatcher)
+                .SetPanelControllerFactory(panelControllerFactory)
+                .Build();
+            
+            containerBuilder.AddSingleton(panelManager, typeof(IPanelManager));
+            
+            containerBuilder.OnContainerBuilt += container =>
+            {
+                panelControllerFactory.SetContainer(container);
+            };
+        }
+    }
+}
+
+#endif
