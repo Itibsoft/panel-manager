4ee4a39ba26877bf97c5ee27b2972d6ea9553ee9
Mrrebrik <itibsoft@gmail.com>
2024-03-23 15:38:01 +0700

    - update: logic for sample reflex

---
+++ Samples~/Reflex/Scripts/MainInstaller.cs
@@ -0,0 +1,14 @@
+﻿using Itibsoft.PanelManager.External;
+using Reflex.Core;
+using UnityEngine;
+
+namespace Itibsoft.PanelManager.Sample.Demo
+{
+    public class MainInstaller : MonoBehaviour, IInstaller
+    {
+        public void InstallBindings(ContainerBuilder containerBuilder)
+        {
+            PanelManagerInstaller.Install(containerBuilder);
+        }
+    }
+}
